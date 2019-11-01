19cba01c0a5caaa2dc7168f17d62b1b97b1a19f0
tangbo <bo.tang3@56qq.com>
2019-11-01 14:27:36 +0800

    feat: add some code

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const fs = require('fs');
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  fs.readdir('dist', (err, files) => {
+    console.log('s', files);
+    files.forEach(file => {
+      console.log('name:', file);
+    });
+    core.setOutput("time", time);
+  });
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
